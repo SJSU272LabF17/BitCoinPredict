3c50da127a424dd1d971ee34364919226c73bf94
abhineet1991 <32442416+abhineet1991@users.noreply.github.com>
2017-12-23 11:11:00 -0800

    Pulling changes from 272 lab repo

---
+++ tweetwise_config.py
@@ -0,0 +1,4 @@
+CONSUMER_KEY = # your consumer key 
+CONSUMER_SECRET = # your consumer secret key 
+ACCESS_TOKEN = 	# your twitter API access token
+ACCESS_TOKEN_SECRET = # your twitter API access token secret 
